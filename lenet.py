b7ab7153cba4e2e4b45d40b862279b848470c66a
samin.karki1 <samin.karki@appliflo.com>
2022-07-16 12:08:10 +0545

    basic lenet implementation on mnist dataset

---
+++ lenet.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 15 08:58:37 2022
+lenet Implementation
+
+@author: samin
+"""
+
+import tensorflow as tf
+import tensorflow.keras as keras
+from keras.datasets import mnist
+import matplotlib.pyplot as plt
+import numpy as np
+from keras import layers
+from keras.layers import Conv2D,AveragePooling2D, Flatten, Dense
+from keras import optimizers, losses
+
+
+EPOCH = 10
+BATCH_SIZE = 128
+
+def preprocess():
+    pass
+
+def show_input():
+    pass
+
+def Lenet():
+    pass
+
+def plot_confusion_matrix():
+    pass
+
+def predict():
+    pass
+
+def save_model():
+    pass
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+print(x_train.shape, y_train.shape)
+print(x_test.shape , y_test.shape)
+
+#reshaping
+x_train=x_train.reshape((x_train.shape[0],x_train.shape[1],x_train.shape[2],1))
+x_test=x_test.reshape((x_test.shape[0],x_test.shape[1],x_test.shape[2],1))
+
+print(x_train.shape, y_train.shape)
+print(x_test.shape , y_test.shape)
+
+#normalizing
+x_train=x_train.astype('float')/255.0
+x_test = x_test.astype('float')/255.0
+
+#plotting
+fig = plt.figure(figsize=(10,10))
+for i in range(25):
+    ax= fig.add_subplot(5, 5, i+1, xticks=[], yticks=[])
+
+    ax.imshow(np.squeeze(x_train[i]), cmap='gray')
+
+    ax.set_title(y_train[i])
+    
+#check the shape
+print(x_train.shape[1:])
+shape = x_train.shape[1:]
+
+#create a model
+model = keras.Sequential()
+model.add(Conv2D(filters=6, kernel_size=(5,5), activation='relu', input_shape=shape))
+model.add(AveragePooling2D())
+model.add(Conv2D(filters=16, kernel_size=(5,5), activation='relu'))
+model.add(AveragePooling2D())
+model.add(Flatten())
+model.add(Dense(units=120, activation='relu'))
+model.add(Dense(units=84, activation='relu'))
+model.add(Dense(units=10, activation='softmax'))
+
+#compile the model
+model.compile(optimizer=optimizers.SGD(lr=0.01), 
+             loss='sparse_categorical_crossentropy',
+             metrics=['accuracy'])
+
+model.summary()
+
+x=model.fit(x_train, y_train, epochs=25, batch_size = 128, verbose= 2 , validation_split = 0.3)
+
+loss, accuracy= model.evaluate(x_test, y_test, verbose = 0)
+
+print(f'Accuracy: {accuracy*100}')
+
+def summary_history(history):
+  plt.figure(figsize = (10,6))
+  plt.plot(history.history['accuracy'], color = 'blue', label = 'train')
+  plt.plot(history.history['val_accuracy'], color = 'red', label = 'val')
+  plt.legend()
+  plt.title('Accuracy')
+  plt.show()
+
+summary_history(x)
+
+
+
+import numpy as np
+# predict labels for the test set
+y_test_pred = []
+range_val=len(x_test)
+for i in range(range_val):
+  img = x_test[i]
+  #print(img)
+  img = img.reshape(1,28,28,1)
+  #img = img.astype('float32')
+  # one-hot vector output
+  vec_p = model.predict(img)
+  # determine the lable corresponding to vec_p
+  y_p = np.argmax(vec_p)
+  print(vec_p, y_p)
+  y_test_pred.append(y_p)
+
+y_test_pred = np.asarray(y_test_pred)
+
+from sklearn.metrics import confusion_matrix
+import seaborn as sns 
+#print(y_test[:range_val], y_test_pred[:range_val])
+con_mat = confusion_matrix(y_test[:range_val], y_test_pred)
+
+plt.figure(figsize = (8,6))
+sns.heatmap(con_mat, linewidths = 0.1, cmap = 'Greens', linecolor = 'gray', fmt = '.1f', annot = True)
+plt.xlabel('Predicted classes', fontsize = 20)
+plt.ylabel('True classes', fontsize = 20)
