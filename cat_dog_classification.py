4e721e9ea09439daa819cec7bf19ba983956b4dd
samnkrki <samn_krki@live.com>
2022-08-23 11:54:43 +0545

    load and predict fn added

--- cat_dog_classification.py
+++ cat_dog_classification.py
@@ -17,6 +17,9 @@ from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_i
 import pandas as pd
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+import matplotlib.gridspec as gridspec
 
 def get_dataset(dataset_root):
     train_folder = [];
@@ -90,7 +93,8 @@ model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['ac
 IMG_WIDTH = 224
 IMG_HEIGHT=224
 BATCH_SIZE=320
-EPOCHS=50
+EPOCHS=5
+IMG_SIZE=(IMG_WIDTH, IMG_HEIGHT)
 
 #data prepration
 train_datagen = ImageDataGenerator(preprocessing_function=tf.keras.applications.inception_v3.preprocess_input, rotation_range=40, width_shift_range=0.2, height_shift_range=0.2, shear_range=0.2, zoom_range=0.2, horizontal_flip=True, fill_mode='nearest')
@@ -101,7 +105,55 @@ validation_generator = train_datagen.flow_from_dataframe(test_df, x_col='filepat
 
 STEPS_PER_EPOCH=32
 VALIDATION_STEPS=6
+MODEL_FILE = 'outputs/cat_dog_classification.model'
 
 history = model.fit(train_generator, epochs=EPOCHS, validation_steps=6)
 
+model.save(MODEL_FILE)
+#plot training
+def plot_training(history):
+    acc = history.history['accuracy']
+    #val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    #val_loss = history.history['val_loss']
+    epochs = range(len(acc))
+    
+    plt.plot(epochs, acc, 'r.')
+    #plt.plot(epochs, val_acc, 'r')
+    plt.title('Training and validation loss')
+    plt.show()
+
+plot_training(history)
+print(history.history)
+#prediction 
+
+def predict(model, img):
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    preds = model.predict(x)
+    return preds[0]
+
+def plot_pred(img, preds):
+    labels = ["cat", "dog"]
+    gs = gridspec.GridSpec(2, 1, height_ratios=[4,1])
+    plt.figure(figsize=(8,8))
+    plt.subplot(gs[0])
+    plt.imshow(np.asarray(img))
+    plt.subplot(gs[1])
+    plt.barh([0,1], preds, alpha=0.5)
+    plt.yticks([0,1], labels)
+    plt.xlabel('Probability')
+    plt.xlim(0,1)
+    plt.tight_layout()
+    
+model = load_model(MODEL_FILE)
+
+pred_src='assets/lion.jpg'
+
+
+img = image.load_img(pred_src, target_size=IMG_SIZE)
+preds = predict(model, img)
+plot_pred(np.asarray(img), preds)
+preds
 '''        '''
